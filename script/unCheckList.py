c000c2ca01ddf87b5d318e7bd1b3e303c4d3e690
keleqnma <cyq65536@gmail.com>
2021-01-29 16:16:31 +0800

    feat： 增加日志记录功能

--- script/unCheckList.py
+++ script/unCheckList.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+from __future__ import print_function
 import codecs
 import sys
 import time
+import datetime
+import os
 
-f = open('Check-inlist.txt', 'r')
-check_in_list = [line.strip() for line in f]
-for index, ch in enumerate(check_in_list):
-    check_in_list[index] = str(ch).decode('utf-8')
-f.close()
+
+def read_chinese_file(filepath):
+    f = open(filepath, 'r')
+    lines = [line.strip() for line in f]
+    for index, ch in enumerate(lines):
+        lines[index] = str(ch).decode('utf-8')
+    f.close()
+    return lines
+
+
+check_in_list = read_chinese_file('Check-inlist.txt')
 
 checked_names = sys.stdin.readlines()
 for index, ch in enumerate(checked_names):
     checked_names[index] = str(ch).decode('utf-8')
 uncheck_list = []
-
-notification = "滴滴滴打卡提示！现在是北京时间："
-output = "，请以下未完成打卡的姑娘尽量完成打卡哦"
-print ("\n\n"+notification.decode('utf-8')+time.strftime(
-    "%Y-%m-%d %H:%M:%S", time.localtime())+output.decode('utf-8'))
-
 for name in check_in_list:
     # 移除开头
     extract_name = name
@@ -38,7 +41,55 @@ for name in check_in_list:
             found = True
             break
     if found == False:
-        uncheck_list.append(name)
+        uncheck_list.append(extract_name)
+
+notification = "滴滴滴打卡提示！现在是北京时间："
+output = "，请以下未完成打卡的姑娘尽量完成打卡哦"
+print ("\n\n"+notification.decode('utf-8')+time.strftime(
+    "%Y-%m-%d %H:%M:%S", time.localtime())+output.decode('utf-8'))
 
 for uncheck_name in uncheck_list:
-    print(uncheck_name)
+    print('@'+uncheck_name)
+
+# 写入当天打卡记录，判断时间是否早于当天七点，如果早于，则是前一天的打卡
+end_check_time = datetime.datetime.strptime(
+    str(datetime.datetime.now().date())+'7:00', '%Y-%m-%d%H:%M')
+
+time_now = datetime.datetime.now()
+
+if time_now < end_check_time:
+    check_day = time_now - \
+        datetime.timedelta(days=1).strftime('%Y-%m-%d')
+else:
+    check_day = time_now.strftime('%Y-%m-%d')
+
+check_log_f = open('log/'+check_day + ' checked.txt', 'w')
+for checked_name in checked_names:
+    check_log_f.write(checked_name.encode('utf-8'))
+check_log_f.close()
+
+uncheck_log_f = open('log/'+check_day + ' unchecked.txt', 'w')
+for uncheck_name in uncheck_list:
+    uncheck_log_f.write(uncheck_name.encode('utf-8'))
+    uncheck_log_f.write('\n')
+uncheck_log_f.close()
+
+# 输出未打卡历史记录
+uncheck_dict = {}
+path = os.getcwd()+'/log'
+files = os.listdir(path)
+for file in files:
+    if 'unchecked' in file:
+        names = read_chinese_file(path + '/' + file)
+        for name in names:
+            if name in uncheck_dict:
+                uncheck_dict[name] = uncheck_dict[name] + 1
+            else:
+                uncheck_dict[name] = 1
+uncheck_thre = 5
+print ('\n'+"多次未打卡名单".decode('utf-8'))
+for key, value in uncheck_dict.items():
+    if value >= uncheck_thre:
+        print (key, end=' ')
+        print (value, end='')
+        print ("次".decode('utf-8'))
