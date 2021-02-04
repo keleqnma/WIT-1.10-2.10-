ef311bffcdff52e290834fe1a3dbff04d22cdf1b
keleqnma <cyq65536@gmail.com>
2021-02-04 15:57:39 +0800

    log: 0131 0201 0203 记录

--- script/unCheckList2.py
+++ script/unCheckList2.py
@@ -8,7 +8,6 @@ import datetime
 import os
 
 
-
 def read_chinese_file(filepath):
     f = open(filepath, 'r')
     lines = [line.strip() for line in f]
@@ -17,6 +16,7 @@ def read_chinese_file(filepath):
     f.close()
     return lines
 
+
 def Findaite(message):
     i = 0
     for charater in message:
@@ -24,12 +24,12 @@ def Findaite(message):
             return i
         i = i+1
 
+
 f = open('Check-inlist.txt', encoding='UTF-8')
 check_in_list = [line.strip() for line in f]
 for index, ch in enumerate(check_in_list):
     check_in_list[index] = str(ch)
 f.close()
-# print(check_in_list)
 
 # 将当天打卡名单复制到testfile.txt中，读入该文件到checked_names
 daka_f = open('testfile.txt', encoding='UTF-8')
@@ -39,7 +39,6 @@ checked_names = [line.strip() for line in daka_f]
 for index, ch in enumerate(checked_names):
     checked_names[index] = str(ch)
 uncheck_list = []
-
 
 
 for name in check_in_list:
@@ -72,10 +71,10 @@ for index, uncheck_name in enumerate(uncheck_list):
     uncheck_list[index] = uncheck_name[i+1:]
     print('@'+uncheck_list[index])
 
-### new here
-# 写入当天打卡记录，判断时间是否早于当天七点，如果早于，则是前一天的打卡
+# new here
+# 写入当天打卡记录，判断时间是否早于当天十二点，如果早于，则是前一天的打卡
 end_check_time = datetime.datetime.strptime(
-    str(datetime.datetime.now().date())+'7:00', '%Y-%m-%d%H:%M')
+    str(datetime.datetime.now().date())+'12:00', '%Y-%m-%d%H:%M')
 
 time_now = datetime.datetime.now()
 
@@ -87,13 +86,13 @@ else:
 
 check_log_f = open('log/'+check_day + ' checked.txt', 'w')
 for checked_name in checked_names:
-    check_log_f.write(checked_name) # .encode('utf-8')
+    check_log_f.write(checked_name)  # .encode('utf-8')
     check_log_f.write('\n')
 check_log_f.close()
 
 uncheck_log_f = open('log/'+check_day + ' unchecked.txt', 'w')
 for uncheck_name in uncheck_list:
-    uncheck_log_f.write(uncheck_name) # .encode('utf-8')
+    uncheck_log_f.write(uncheck_name)  # .encode('utf-8')
     uncheck_log_f.write('\n')
 uncheck_log_f.close()
 
@@ -111,11 +110,11 @@ for file in files:
                 uncheck_dict[name] = 1
 
 # 按未打卡次数排序
-uncheck_thre = 7
+uncheck_thre = len(files) / 4 + 1
 sorted_pairs = sorted(uncheck_dict.items(), key=lambda kv: kv[1])
-print ('\n'+"多次未打卡名单") # .decode('utf-8'))
+print('\n'+"多次未打卡名单")
 for pair in sorted_pairs:
     if pair[1] >= uncheck_thre:
-        print (pair[0], end=' ')
-        print (pair[1], end='')
-        print ("次") # .decode('utf-8')
+        print(pair[0], end=' ')
+        print(pair[1], end='')
+        print("次")
